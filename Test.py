fa86007b09e943c6cacfe07ffcfadcfec0349624
manaswini <manaswinisunkari2000@gmail.com>
2024-05-28 20:34:08 +0100

    Deleted the unwanted print data frame statement

--- Test.py
+++ Test.py
@@ -1,6 +1,5 @@
 import pandas as pd
 df = pd.read_csv("tsv.txt", sep="\s")
-df.to_csv('tsv,txt',header=df.columns, index=None, sep=' ')
 print(df.loc[1:3])
 df.replace(1.4, None, inplace = True)
 print(df)
