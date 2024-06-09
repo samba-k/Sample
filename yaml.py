e3eebe4adcfdc4dd4cb510f5047ad5ca0f0f8676
manaswini <manaswinisunkari2000@gmail.com>
2024-06-09 23:20:27 +0100

    Creadted a script to modify yaml file and write the modified data to yaml

---
+++ yaml.py
@@ -0,0 +1,12 @@
+import yaml
+
+with open("eg.yaml", "r")as file:
+    result = yaml.safe_load(file)
+# print(type(result))
+result['Languages'] = {'python' : '4', 'java' : '3', 'c++' : '2', 'go' : '1'}
+# print(result) 
+
+with open("eg.yaml", "w")as file:
+    yaml.dump(result, file, sort_keys=True)
+print(result)
+
