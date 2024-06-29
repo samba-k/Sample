225ce8ab62786ee1b59f411bf4aaad0cf30e035d
Manaswinisunkari <168231815+Manaswinisunkari@users.noreply.github.com>
2024-06-29 14:32:33 +0100

    Update getp.py

--- getp.py
+++ getp.py
@@ -3,6 +3,6 @@ try:
         p=getpass.getpass()
 except Exception as error:
         print('ERORR', error)
-    else:
-            print('entered password is:', p)
+else:
+        print('entered password is:', p)
 print("Getpass tested")
